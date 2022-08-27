c82c718ff50305b72b00edc7113360c6f519ccd9
MinFTW <kimmins2@outlook.com>
2022-08-27 09:40:44 -0500

    refined app.js

--- app.js
+++ app.js
@@ -1,9 +1,4 @@
-// DOM ELEMENTS
-let message = document.getElementById('message');
-let subMessage = document.getElementById('subMessage');
-let guessCounter = document.getElementById('guessesLeft');
-
-// HELPER FUNCTIONS
+// GAME
 function newGame() {
   winningNumber = generateWinningNumber(1, 100);
   playersGuess = null;
@@ -22,13 +17,14 @@ function newGame() {
   hintButton.style.backgroundColor = '#8aacff';
   guessCounter.innerText = 5;
 
-  for (let i = 0; i < 4; i++) {
-    let idName = 'guess' + i.toString();
-    let pastGuesses = document.getElementById(idName);
+  for (let i = 0; i < guessesLeft; i++) {
+    let id = 'guess' + i.toString();
+    let pastGuesses = document.getElementById(id);
     pastGuesses.innerText = '-';
   }
 }
 
+// HELPER FUNCTIONS
 function generateWinningNumber(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
@@ -36,7 +32,74 @@ function generateWinningNumber(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-function shuffle(array) {
+function checkPlayerInput() {
+  let guess = document.getElementById('playersGuess').value;
+
+  playersGuess = parseInt(guess);
+
+  if (
+    !Number.isInteger(playersGuess) ||
+    playersGuess < 1 ||
+    playersGuess > 100
+  ) {
+    message.innerText = `Not a valid guess. Please try again.`;
+    subMessage.innerText = `Enter a number between 1 - 100`;
+    return;
+  }
+
+  checkGuess();
+  setPastGuess();
+}
+
+function checkGuess() {
+  if (playersGuess === winningNumber) {
+    message.innerText = `You Win! Winning Number was ${winningNumber}.`;
+    subMessage.innerText = `Press reset to play again!`;
+    return gameOver();
+  } else if (guessesLeft <= 0) {
+    message.innerText = `You Lose. Winning number was ${winningNumber}.`;
+    subMessage.innerText = `Press reset to play again!`;
+    guessCounter.innerText = 0;
+    return gameOver();
+  } else if (pastGuesses.includes(playersGuess)) {
+    message.innerText = `You already guessed that number.`;
+  } else {
+    pastGuesses.push(playersGuess);
+    guessCounter.innerText = guessesLeft--;
+    return heatCheck();
+  }
+
+  return;
+}
+
+function heatCheck() {
+  const difference = Math.abs(playersGuess - winningNumber);
+  const isLower = playersGuess < winningNumber ? true : false;
+
+  isLower
+    ? (subMessage.innerText = `Guess higher!`)
+    : (subMessage.innerText = `Guess lower!`);
+
+  if (difference < 10) {
+    message.innerText = `You're burning up!`;
+  } else if (difference < 25) {
+    message.innerText = `You're lukewarm.`;
+  } else if (difference < 50) {
+    message.innerText = `You're a bit chilly.`;
+  } else if (difference < 100) {
+    message.innerText = `You ice cold!`;
+  }
+}
+
+function setPastGuess() {
+  for (let i = 0; i < pastGuesses.length; i++) {
+    let id = 'guess' + i.toString();
+    let element = document.getElementById(id);
+    element.innerText = pastGuesses[i].toString();
+  }
+}
+
+function shuffleHints(array) {
   let m = array.length,
     t,
     i;
@@ -52,77 +115,7 @@ function shuffle(array) {
   return array;
 }
 
-let difference = () => Math.abs(playersGuess - winningNumber);
-let isLower = () => (playersGuess < winningNumber ? true : false);
-
-function getGuess() {
-  let playersGuessRaw = document.getElementById('playersGuess').value;
-
-  return parseInt(playersGuessRaw);
-}
-
-function playersGuessSubmission() {
-  playersGuess = getGuess();
-
-  if (
-    !Number.isInteger(playersGuess) ||
-    playersGuess < 1 ||
-    playersGuess > 100
-  ) {
-    message.innerText = `This is an invalid guess.`;
-    subMessage.innerText = `Enter a number between 1 - 100`;
-    throw `This is an invalid guess.`;
-  }
-
-  checkGuess();
-  setPreviousGuesses();
-}
-
-function checkGuess() {
-  if (playersGuess === winningNumber) {
-    message.innerText = `You Win! Winning Number was ${winningNumber}.`;
-    subMessage.innerText = `Press reset to play again!`;
-    gameOver();
-    return;
-  } else if (pastGuesses.length >= 4) {
-    message.innerText = `You Lose. Winning number was ${winningNumber}.`;
-    subMessage.innerText = `Press reset to play again!`;
-    guessCounter.innerText = 0;
-    gameOver();
-    return;
-  } else if (pastGuesses.includes(playersGuess)) {
-    message.innerText = `You already guessed that number.`;
-  } else {
-    pastGuesses.push(playersGuess);
-    if (isLower()) {
-      subMessage.innerText = `Guess higher!`;
-    } else if (!isLower()) {
-      subMessage.innerText = `Guess lower!`;
-    }
-    if (difference() < 10) {
-      message.innerText = `You're burning up!`;
-    } else if (difference() < 25) {
-      message.innerText = `You're lukewarm.`;
-    } else if (difference() < 50) {
-      message.innerText = `You're a bit chilly.`;
-    } else if (difference() < 100) {
-      message.innerText = `You ice cold!`;
-    }
-    guessCounter.innerText = guessesLeft--;
-  }
-
-  return;
-}
-
-function setPreviousGuesses() {
-  for (let i = 0; i < pastGuesses.length; i++) {
-    let idName = 'guess' + i.toString();
-    let element = document.getElementById(idName);
-    element.innerText = pastGuesses[i].toString();
-  }
-}
-
-function getHint() {
+function provideHint() {
   let hintArray = [winningNumber];
 
   while (hintArray.length < 3) {
@@ -133,7 +126,7 @@ function getHint() {
       hintArray.push(hint);
   }
 
-  shuffle(hintArray);
+  shuffleHints(hintArray);
   message.innerText = `The winning number is one of these: ${hintArray}.`;
 
   document.getElementById('hint').disabled = true;
@@ -149,23 +142,28 @@ function gameOver() {
   hintButton.style.backgroundColor = 'lightgray';
 }
 
-// EVENT LISTENERS
-let submitButton = document.getElementById('submit');
-submitButton.addEventListener('click', playersGuessSubmission);
+// DOM ELEMENTS
+const message = document.getElementById('message');
+const subMessage = document.getElementById('subMessage');
+const guessCounter = document.getElementById('guessesLeft');
 
-let resetButton = document.getElementById('reset');
+// EVENT LISTENERS
+const submitButton = document.getElementById('submit');
+submitButton.addEventListener('click', checkPlayerInput);
+
+const resetButton = document.getElementById('reset');
 resetButton.addEventListener('click', newGame);
 
-let hintButton = document.getElementById('hint');
-hintButton.addEventListener('click', getHint);
+const hintButton = document.getElementById('hint');
+hintButton.addEventListener('click', provideHint);
 
-let inputField = document.getElementById('playersGuess');
+const inputField = document.getElementById('playersGuess');
 inputField.addEventListener('focus', function () {
   document.getElementById('playersGuess').value = '';
 });
 
 inputField.addEventListener('change', function () {
-  playersGuessSubmission();
+  checkPlayerInput();
   document.getElementById('playersGuess').value = '';
 });
 
